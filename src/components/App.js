ef324dc324567f48ae0a492a2c5aed9523489b6a
Lucas Duncan <lucasdonuts@gmail.com>
2022-07-25 14:17:19 -0500

    commit tree bb302c620ad3a9dc19cc5cd85376b7b1f84d4dc6

--- src/components/App.js
+++ src/components/App.js
@@ -1,4 +1,4 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 
 import Filters from "./Filters";
 import PetBrowser from "./PetBrowser";
@@ -6,6 +6,36 @@ import PetBrowser from "./PetBrowser";
 function App() {
   const [pets, setPets] = useState([]);
   const [filters, setFilters] = useState({ type: "all" });
+  // const [filterSelected, setFilterSelected] = useState(false);
+
+  const updateFilters = (e) => {
+    setFilters( { type: e.target.value } );
+  }
+
+  const onAdoptPet = (adoptedPetId) => {
+
+    const updatedPets = pets.map( pet => {
+      if( pet.id === adoptedPetId ) {
+        pet.isAdopted = true;
+      }
+      return pet;
+    });
+
+    setPets(updatedPets);
+  }
+
+  const findPets = () => {
+    let trailingUrl = '';
+
+    if(filters.type !== 'all') {
+      trailingUrl = `?type=${filters.type}`
+    }
+
+    fetch(`http://localhost:3001/pets${trailingUrl}`)
+      .then( res => res.json() )
+      .then( setPets )
+
+  }
 
   return (
     <div className="ui container">
@@ -15,10 +45,16 @@ function App() {
       <div className="ui container">
         <div className="ui grid">
           <div className="four wide column">
-            <Filters />
+            <Filters
+              onFindPetsClick={ findPets }
+              onChangeType={ updateFilters }
+            />
           </div>
           <div className="twelve wide column">
-            <PetBrowser />
+            <PetBrowser
+              pets={ pets }
+              onAdoptPet={ onAdoptPet }
+            />
           </div>
         </div>
       </div>
